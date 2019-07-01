e76d085bea04d7e8da707b967186040e5ac668e2
LeCare <francois.caron.perso@gmail.com>
2019-07-01 21:32:57 +0200

    Rebuild framebuffer on resize

--- yabause/src/vidogl.c
+++ yabause/src/vidogl.c
@@ -3412,14 +3412,12 @@ void VIDOGLResize(int originx, int originy, unsigned int w, unsigned int h, int 
   GlWidth = w;
   GlHeight = h;
 
-  setupMaxSize();
+  SetSaturnResolution(vdp2width, vdp2height);
 
   _Ygl->originx = originx;
   _Ygl->originy = originy;
 
   glViewport(originx, originy, GlWidth, GlHeight);
-
-//  SetSaturnResolution(vdp2width, vdp2height);
 
 }
 
